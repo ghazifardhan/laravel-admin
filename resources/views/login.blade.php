45cfa213a661ce7bd12850e105aa3ef0a89cb204
Ghazi Fadil <ghazifardhan@gmail.com>
2019-02-18 22:51:46 +0700

    update

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -25,7 +25,13 @@
 <body class="hold-transition login-page" @if(config('admin.login_background_image'))style="background: url({{config('admin.login_background_image')}}) no-repeat;background-size: cover;"@endif>
 <div class="login-box">
   <div class="login-logo">
-    <a href="{{ admin_base_path('/') }}"><b>{{config('admin.name')}}</b></a>
+    <img 
+      src="{{ asset('Logo_Dekranasda.png') }}" 
+      alt="Admin logo" 
+      style="width: 70%;height: 70%;">
+    <a href="{  { admin_base_path('/') }}">
+      {{config('admin.name')}}
+    </a>
   </div>
   <!-- /.login-logo -->
   <div class="login-box-body">
